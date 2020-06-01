8ea7f091e8edf37999c2a448c6f739dae1ce3dad
Jordan Noe <jordanvinnoe@gmail.com>
2020-05-31 21:22:18 -0400

    sceeds refactored

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Meatball sub');
+INSERT INTO burgers (name) VALUES ('Extra cheese');
+INSERT INTO burgers (name, eaten) VALUES ('Black bean burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Fat Sandwich', true);
+INSERT INTO burgers (name, eaten) VALUES ('Royale with cheese', true);
+INSERT INTO burgers (name) VALUES ('Taco burger');
